75d2000dcdbe100c98ff0db30915371ef480e585
Hendra Hartanto <133342018+hendrahartanto@users.noreply.github.com>
2026-05-22 00:41:23 +0700

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.cpp
@@ -0,0 +1,22 @@
+/*
+loop every element simultaneously from first to end
+if in the current loop every element have the same character push the characterto result string
+*/
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = strs[0];
+        for (int i = 1; i < strs.size(); i++) {
+            int j = 0;
+            while (j < min(prefix.length(), strs[i].length())) {
+                if (prefix[j] != strs[i][j]) {
+                    break;
+                }
+                j++;
+            }
+            prefix = prefix.substr(0, j);
+        }
+        return prefix;
+    }
+};
