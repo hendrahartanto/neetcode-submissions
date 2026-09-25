097f4462ca2f9ec39442b245f8faf9871e3bf7c6
Hendra Hartanto <133342018+hendrahartanto@users.noreply.github.com>
2026-09-25 10:12:35 +0700

    Add: design-hashmap - submission-3

---
+++ Data Structures & Algorithms/design-hashmap/submission-3.cpp
@@ -0,0 +1,34 @@
+/*
+initial solve
+-------------
+because the key is integer we can use it derectly as an index
+*/
+
+class MyHashMap {
+private:
+    vector<int> hashMap;
+public:
+    MyHashMap() {
+        hashMap = vector<int>(1000001, -1);
+    }
+    
+    void put(int key, int value) {
+        hashMap[key] = value;
+    }
+    
+    int get(int key) {
+        return hashMap[key];
+    }
+    
+    void remove(int key) {
+        hashMap[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
